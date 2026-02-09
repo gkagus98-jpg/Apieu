cd356f12d5afa08607c68308e1703afe0539437d
한혜진 <gkagus98@naver.com>
2026-02-09 23:53:10 +0900

    메뉴 추가

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(function () {
+
   $("#headers").load("common/header.html", function (res, status) {
     if (status === "error") {
       console.error("헤더 로드 실패");
@@ -10,4 +11,47 @@ $(function () {
       console.error("푸터 로드 실패");
     }
   });
+
+  $("#menus").load("common/menu.html", function (res, status) {
+    if (status === "error") {
+      console.error("메뉴 로드 실패");
+    }
+  });
+
+
+  // 메뉴 열기
+  $(document).on("click", ".menu_open a", function (e) {
+    e.preventDefault();
+    $(".menu_wrap").addClass("active");
+  });
+
+  // 메뉴 닫기
+  $(document).on("click", ".quick_menu li:last-child a", function (e) {
+    e.preventDefault();
+    $(".menu_wrap").removeClass("active");
+
+    $(".submenu").slideUp();
+    $("#gnb > ul > li > a img").attr("src", "images/icn_plus.png");
+  });
+
+  // 아코디언 메뉴
+  $(document).on("click", "#gnb > ul > li > a", function (e) {
+    e.preventDefault();
+
+    let $this = $(this);
+    let $submenu = $this.next(".submenu");
+
+    if ($submenu.is(":visible")) {
+      $submenu.slideUp();
+      $this.find("img").attr("src", "images/icn_plus.png");
+      return;
+    }
+
+    $(".submenu").slideUp();
+    $("#gnb > ul > li > a img").attr("src", "images/icn_plus.png");
+
+    $submenu.slideDown();
+    $this.find("img").attr("src", "images/icn_minus.png");
+  });
+
 });
